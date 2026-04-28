21a83d040ccfe52a50e3ca0a7c58764914f2a243
harsh <hrrshpal@gmail.com>
2026-04-28 11:47:18 +0000

    added educational experience component

--- src/components/EducationalExperience.jsx
+++ src/components/EducationalExperience.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const EducationalExperience = () => {
+    const[educationList, setEducationList] = useState([1]);
+
+
+    function addEducation(){
+        setEducationList([...educationList, educationList.length+1])
+    }
+
+    function deleteEducation(){
+        educationList.pop()
+        setEducationList([...educationList])
+    }
+
   return (
-    <div>
+    <>
         <h2>Educational Experience</h2>
-        
-    </div>
+        {
+            educationList.map(item => <EducationSection key={item} />)
+        }
+        <button onClick={addEducation}>Add Section (+)</button>
+        <button onClick={deleteEducation}>Delete Section (-)</button>
+    </>
   )
+}
+
+const EducationSection = () => {
+
+    function handleChange(){}
+
+    return <>
+        <div>
+            <input onChange={handleChange} name="degree" required type="text" className="degree" placeholder='e.g. Bachelor of Computer Applications'/>
+            <input onChange={handleChange} name="school" required type="text" className="school" placeholder='e.g. Delhi University' />
+            <input onChange={handleChange} name="city" required type="text" className="city" placeholder='e.g. New Delhi' />
+            <input onChange={handleChange} name="startdate" type="date" className="startdate" />
+            <input onChange={handleChange} name="enddate" type="date" className="enddate" />
+            <textarea onChange={handleChange} name="description" type="textarea" className="description" placeholder='Description (Optional)' />
+        </div>
+    </>
 }
 
 export default EducationalExperience
--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -17,20 +17,20 @@ const GeneralInformation = () => {
         newObj.name = value
         setInfo(newObj)
     }
-
-    console.log(info)
     
   return (
-    <div>
+    <>
         <h2>General Information</h2>
-        <input onChange={handleChange} name="name" required type="text" className="name" placeholder='John Doe'/>
-        <input onChange={handleChange} name="email" required type="text" className="email" placeholder='abc@xyz.com' />
-        <input onChange={handleChange} name="phone" required type="tel" className="phone" placeholder='+91-XXXXX-XXXXX' />
-        <input onChange={handleChange} name="address" required type="text" className="address" placeholder='Jane Street' />
-        <input onChange={handleChange} name="city" required type="text" className="city" placeholder='Gotham City' />
-        <input onChange={handleChange} name="zipcode" required type="number" className="zipcode" placeholder='XXXXXX' />
-        <textarea onChange={handleChange} name="summary" required type="textarea" className="summary" placeholder='My Summary' />
-    </div>
+        <div>
+            <input onChange={handleChange} name="name" required type="text" className="name" placeholder='John Doe'/>
+            <input onChange={handleChange} name="email" required type="text" className="email" placeholder='abc@xyz.com' />
+            <input onChange={handleChange} name="phone" required type="tel" className="phone" placeholder='+91-XXXXX-XXXXX' />
+            <input onChange={handleChange} name="address" required type="text" className="address" placeholder='Jane Street' />
+            <input onChange={handleChange} name="city" required type="text" className="city" placeholder='Gotham City' />
+            <input onChange={handleChange} name="zipcode" required type="number" className="zipcode" placeholder='XXXXXX' />
+            <textarea onChange={handleChange} name="summary" required type="textarea" className="summary" placeholder='My Summary' />
+        </div>
+    </>
   )
 }
 
